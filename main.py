fddcc39ddcf2b5ce3f50d83dd1aa82a60f183276
ida <idahoney@gmx.at>
2020-04-13 17:53:20 +0200

    get prototype working (more or less)

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pathlib
 import numpy as np
 from keras.preprocessing.image import load_img, img_to_array
 import cv2
+from PIL import Image
 
 origin = 'file:///home/ida/.keras/datasets/cat10-dataset.zip'
 fname = 'cat10-dataset'
@@ -16,9 +17,8 @@ def load_images():
     data_dir = pathlib.Path(data_dir)
 
     images = []
-    for file in list(data_dir.glob('*.jpg')):
-        print(file.as_posix())
-        img = load_img(file.as_posix(), color_mode="grayscale")
+    for file in list(data_dir.glob('*.jpg'))[0:5]:
+        img = load_img(file.as_posix(), color_mode="grayscale", target_size=(500, 375))
         img = np.array(img)
         img = img.reshape((500, 375, -1))
         images.append(img)
@@ -27,26 +27,46 @@ def load_images():
 
 def main():
     model = keras.Sequential([
-        keras.layers.Conv2D(3, kernel_size=5, strides=5, input_shape=[500, 375, 1], data_format='channels_last', padding='same'),
+        keras.layers.Conv2D(3, kernel_size=5, strides=5, input_shape=[500, 375, 1], data_format='channels_last', padding='same', activation=keras.activations.relu),
+        keras.layers.Activation(keras.activations.sigmoid),
         keras.layers.MaxPool2D(),
-        keras.layers.Conv2D(3, kernel_size=5, strides=1, padding='same'),
+        keras.layers.Conv2D(3, kernel_size=5, strides=1, padding='same', activation=keras.activations.relu),
+        keras.layers.Activation(keras.activations.sigmoid),
         keras.layers.MaxPool2D(),
         keras.layers.Flatten(),
-        keras.layers.Dense(12),
-        keras.layers.Dense(187500),
+        keras.layers.Dense(100, activation=keras.activations.relu),
+        keras.layers.Dense(187500, activation=keras.activations.sigmoid),
         keras.layers.Reshape([500, 375, 1]),
     ])
 
     print(model.summary())
 
     data = load_images()
-    print(data.shape)
 
-    model.compile(metrics=[keras.metrics.mean_absolute_percentage_error],
+    model.compile(metrics=[keras.metrics.mean_absolute_percentage_error, keras.metrics.mean_absolute_error],
                   loss=keras.losses.mean_absolute_percentage_error,
                   optimizer=keras.optimizers.SGD())
 
-    model.fit(data, data, batch_size=1)
+    model.fit(data, data, batch_size=5, epochs=1000)
+
+    img = data[0]
+    img = np.reshape(img, [1, 500, 375, 1])
+
+    output = model.predict(img)
+
+    output = np.reshape(output[0], [500, 375])
+
+    output *= 255
+
+    pil_img = Image.fromarray(output)
+    pil_img.show()
+
+    img = np.reshape(img, [500, 375])
+
+    img *= 255
+
+    pil_img = Image.fromarray(img)
+    pil_img.show()
 
 
 if __name__ == '__main__':
