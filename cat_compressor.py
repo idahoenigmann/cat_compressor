34214163897daa49861ffd990584522dacb59c26
ida <idahoney@gmx.at>
2020-04-21 08:50:16 +0200

    add gpu option to cat_compressor.py

--- cat_compressor.py
+++ cat_compressor.py
@@ -21,6 +21,11 @@ IMG_WIDTH = 640
 IMG_HEIGHT = 480
 BATCH_SIZE = 50
 
+config = tf.compat.v1.ConfigProto(gpu_options=tf.compat.v1.GPUOptions(per_process_gpu_memory_fraction=0.8))
+config.gpu_options.allow_growth = True
+session = tf.compat.v1.Session(config=config)
+tf.compat.v1.keras.backend.set_session(session)
+
 
 def exit_handler():
     model.save('model_cat_classifier.h5')
