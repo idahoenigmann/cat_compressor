a5d7f417d5fd020ff6bccd7a266b5e8cb0ff90a3
ida <idahoney@gmx.at>
2020-05-06 17:23:32 +0200

    refactor cat_compressor_show_test_img.py code

--- cat_compressor_show_test_img.py
+++ cat_compressor_show_test_img.py
@@ -51,7 +51,7 @@ if __name__ == '__main__':
         model.compile(metrics=[keras.metrics.mean_absolute_percentage_error],
                       loss=keras.losses.mean_absolute_percentage_error,
                       optimizer=keras.optimizers.SGD())
-        data = load_images(0, 10)
+        data = load_images(0, 1)
         for img_idx in range(0, len(data)):
             input_img = np.reshape(data[img_idx], [1, IMG_WIDTH, IMG_HEIGHT, 3])
             output_img = model.predict(input_img)
@@ -66,11 +66,8 @@ if __name__ == '__main__':
             input_img = np.reshape(input_img, [IMG_WIDTH, IMG_HEIGHT, 3])
             pil_input_img = Image.fromarray(np.uint8(input_img))
             pil_input_img.show()
-            continue
+
             diff_img = output_img - input_img
             diff_img = np.abs(diff_img)
-            pil_diff_img = Image.fromarray(diff_img)
-            pil_diff_img = pil_diff_img.resize((IMG_WIDTH * 10, IMG_HEIGHT * 10))
+            pil_diff_img = Image.fromarray(np.uint8(diff_img))
             pil_diff_img.show(title="difference")
-            time.sleep(0.1)
-            pil_diff_img.close()
