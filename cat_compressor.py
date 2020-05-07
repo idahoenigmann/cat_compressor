266e2f1c80aee94dcf3a21bca614e0b149ad5761
ida <idahoney@gmx.at>
2020-05-07 11:07:55 +0200

    add cat_decompressor.py; finish (?) cat_compressor.py :-)

--- cat_compressor.py
+++ cat_compressor.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
 import keras
 import pathlib
-import atexit
 import os.path
+import numpy as np
 
 origin = 'file:///home/sascha/.keras/datasets/cat_faces.zip'
 fname = 'cat_faces'
@@ -10,8 +10,7 @@ model = keras.models.Sequential()
 
 IMG_WIDTH = 640
 IMG_HEIGHT = 480
-BATCH_SIZE = 5
-EPOCHS = 1
+BATCH_SIZE = 3
 
 config = tf.compat.v1.ConfigProto(gpu_options=tf.compat.v1.GPUOptions(per_process_gpu_memory_fraction=0.8))
 config.gpu_options.allow_growth = True
@@ -22,10 +21,6 @@ tf.compat.v1.keras.backend.set_session(session)
 def main():
     entire_model = keras.models.load_model("model_cat_classifier.h5")
 
-    for layer in entire_model.layers:
-        print(layer.get_config(), layer.get_weights())
-
-    return
     model = keras.Sequential([
         keras.layers.Conv2D(8, kernel_size=3, strides=1, input_shape=[IMG_WIDTH, IMG_HEIGHT, 3],
                             data_format='channels_last', padding='same', activation=keras.activations.relu),
@@ -43,7 +38,8 @@ def main():
         keras.layers.Dense(300),
     ])
 
-    print(model.summary())
+    for layer_idx in range(len(model.layers)):
+        model.layers[layer_idx].set_weights(entire_model.layers[layer_idx].get_weights())
 
     model.compile(metrics=[keras.metrics.mean_absolute_percentage_error],
                   loss=keras.losses.mean_absolute_error,
@@ -54,14 +50,17 @@ def main():
         fname=fname, untar=True)
     data_dir = pathlib.Path(data_dir)
 
-    train_dir = os.path.join(data_dir, 'train')
-
-    train_dir_path = pathlib.Path(os.path.join(train_dir, 'train'))
-    cnt_files = len(list(train_dir_path.glob('*.jpg')))
-
+    val_dir = os.path.join(data_dir, 'validation')
     img_generator = keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
-    train_data_gen = img_generator.flow_from_directory(directory=train_dir, target_size=(IMG_WIDTH, IMG_HEIGHT),
-                                                       batch_size=BATCH_SIZE, class_mode="input", shuffle=True)
+    val_data_gen = img_generator.flow_from_directory(directory=val_dir, target_size=(IMG_WIDTH, IMG_HEIGHT),
+                                                     batch_size=BATCH_SIZE, class_mode="input", shuffle=True)
+
+    results = model.predict(val_data_gen, steps=1)
+
+    np.savetxt('data.csv', results, delimiter=',')
+
+    for result in results:
+        print(result)
 
 
 if __name__ == '__main__':
